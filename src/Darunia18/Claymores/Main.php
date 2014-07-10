9d277d0383981b0c6881260a7e3246a47550a309
Darunia18 <darunia1818@gmail.com>
2014-07-09 21:52:25 -0400

    Beginning of 2.1 Update
    Known Bugs:
    -Detecting entity type isn't working yet
    Possibly could have bugs:
    -Haven't been able to test setting BlockDestruction to false in the config yet

--- src/Darunia18/Claymores/Main.php
+++ src/Darunia18/Claymores/Main.php
@@ -2,10 +2,11 @@
 
 namespace Darunia18\Claymores;
 
+use pocketmine\entity;
 use pocketmine\event\entity\EntityMoveEvent;
 use pocketmine\event\Listener;
-use pocketmine\level;
 use pocketmine\level\Explosion;
+use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;
 
@@ -14,6 +15,11 @@ class Main extends PluginBase implements Listener{
     private $explosionSize;
     private $blockDestroy;
     private $activateNearbyClaymores;
+    private $playerActivation;
+    private $mobActivation;
+    private $projectileActivation;
+    private $itemActivation;
+    private $vehicleActivation;
     
     public function onLoad(){
     }
@@ -25,6 +31,11 @@ class Main extends PluginBase implements Listener{
         $this->explosionSize = $config->get("ExplosionSize");
         $this->blockDestroy = $config->get("BlockDestroy");
         $this->activateNearbyClaymores = $config->get("ActivateNearbyClaymores");
+        $this->playerActivation = $config->get("PlayerActivation");
+        $this->mobActivation = $config->get("MobActivation");
+        $this->projectileActivation = $config->get("ProjectileActivation");
+        $this->itemActivation = $config->get("ItemActivation");
+        $this->vehicleActivation = $config->get("VehicleActivation");
         $this->getLogger()->info("Claymores has been enabled.");
 	}
 	
@@ -36,16 +47,85 @@ class Main extends PluginBase implements Listener{
      */
     public function onMove(EntityMoveEvent $event){
         $entity = $event->getEntity();
-        $claymore = $entity->getLevel()->getBlockIdAt($entity->x, ($entity->y -1), $entity->z);
-	if($claymore == $this->claymore){
-            if($this->blockDestroy = true){
-		$explosion = new Explosion(new Position($entity->x, ($entity->y -1), $entity->z, $entity->getLevel()), $this->explosionSize);
-                $explosion->explode();
+        $claymore = $entity->getLevel()->getBlockIdAt($entity->x, ($entity->y -0.5), $entity->z);
+        if($claymore == $this->claymore){
+            if($entity instanceof Human){
+                if($this->playerActivation){
+                    if($this->blockDestroy){
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->explode();
+                    }
+                    else{
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->affectedBlocks = [];
+                        $explosion->explode();
+                        $entity->getLevel()->setBlockIdAt($entity->x, ($entity->y -0.5), $entity->z, 0);
+                    }
+                }
+            }
+            elseif($entity instanceof Living){
+                if($this->mobActivation){
+                    if($entity instanceof Human){
+                        //nothing
+                    }
+                    if($this->blockDestroy){
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->explode();
+                    }
+                    else{
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->affectedBlocks = [];
+                        $explosion->explode();
+                        $entity->getLevel()->setBlockIdAt($entity->x, ($entity->y -0.5), $entity->z, 0);
+                    }
+                }
+            }
+            elseif($entity instanceof Projectile){
+                if($this->projectileActivation){
+                    if($this->blockDestroy){
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->explode();
+                    }
+                    else{
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->affectedBlocks = [];
+                        $explosion->explode();
+                        $entity->getLevel()->setBlockIdAt($entity->x, ($entity->y -0.5), $entity->z, 0);
+                    }
+                }
+            }
+            elseif($entity instanceof DroppedItem){
+                if($this->itemActivation){
+                    if($this->blockDestroy){
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->explode();
+                    }
+                    else{
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->affectedBlocks = [];
+                        $explosion->explode();
+                        $entity->getLevel()->setBlockIdAt($entity->x, ($entity->y -0.5), $entity->z, 0);
+                    }
+                }
+            }
+            elseif($entity instanceof Vehicle){
+                if($this->vehicleActivation){
+                    if($this->blockDestroy){
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->explode();
+                    }
+                    else{
+                        $explosion = new Explosion(new Position($entity->x, ($entity->y -0.5), $entity->z, $entity->getLevel()), $this->explosionSize);
+                        $explosion->affectedBlocks = [];
+                        $explosion->explode();
+                        $entity->getLevel()->setBlockIdAt($entity->x, ($entity->y -0.5), $entity->z, 0);
+                    }
+                }
             }
             else{
-                //Future code goes here
+                $entity->sendMessage("Error");
             }
-	}
+        }
     }
 	
     public function onDisable(){
